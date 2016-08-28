353de7b52b257f5f1572b7f6792517839eeec481
kenz_firespeed <firespeedkenz@gmail.com>
2016-08-28 18:46:40 +0900

    ローカル対応のやり直し

--- service-worker.js
+++ service-worker.js
@@ -1,61 +1,50 @@
 var VERSION = 201608281704;
 var CACHE_NAME = 'cache' + VERSION;
-var DOMAIN = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '');
-var CACHE_URLS = [
-  DOMAIN + '/',
-  DOMAIN + '/index.html',
-  DOMAIN + '/stylesheets/web2016.css',
-  DOMAIN + '/scripts/script.js',
-  DOMAIN + '/images/web2016/btn-arrow.png',
-  DOMAIN + '/images/web2016/main-sp-bg.png',
-  DOMAIN + '/images/web2016/popup-close.png',
-  DOMAIN + '/images/web2016/here.png',
-  DOMAIN + '/images/web2016/main-sp-ttl.png',
-  DOMAIN + '/images/web2016/popup-img.png',
-  DOMAIN + '/images/web2016/icon-blog.png',
-  DOMAIN + '/images/web2016/main.png',
-  DOMAIN + '/images/web2016/icon-twitter.png',
-  DOMAIN + '/images/web2016/nav-title.png',
-  DOMAIN + '/images/web2016profile/13-h-kis.jpeg',
-  DOMAIN + '/images/web2016profile/14-t-matsumana.png',
-  DOMAIN + '/images/web2016profile/17-h-tarunon.jpg',
-  DOMAIN + '/images/web2016profile/13-k-yasukawa.png',
-  DOMAIN + '/images/web2016profile/15-h-taro.jpg',
-  DOMAIN + '/images/web2016profile/17-k-maki.jpg',
-  DOMAIN + '/images/web2016profile/13-t-nagamine.jpg',
-  DOMAIN + '/images/web2016profile/15-t-colun.jpg	',
-  DOMAIN + '/images/web2016profile/17-t-sato.png',
-  DOMAIN + '/images/web2016profile/14-h-yoichiro.jpeg',
-  DOMAIN + '/images/web2016profile/16-h-yokoyama.jpg',
-  DOMAIN + '/images/web2016profile/18-k-hamamoto.jpg',
-  DOMAIN + '/images/web2016profile/14-k-urushibara.jpg',
-  DOMAIN + '/images/web2016profile/16-t-takesako.jpg'];
-var CACHE_FLAG_LIST = {};
-CACHE_URLS.forEach(
-  function(cacheFile){
-    CACHE_FLAG_LIST[cacheFile] = true;
-  }
-);
+var CACHE_FILES = [
+  '/',
+  '/index.html',
+  '/stylesheets/web2016.css',
+  '/javascripts/script.js',
+  '/images/web2016/btn-arrow.png',
+  '/images/web2016/main-sp-bg.png',
+  '/images/web2016/popup-close.png',
+  '/images/web2016/here.png',
+  '/images/web2016/main-sp-ttl.png',
+  '/images/web2016/popup-img.png',
+  '/images/web2016/icon-blog.png',
+  '/images/web2016/main.png',
+  '/images/web2016/icon-twitter.png',
+  '/images/web2016/nav-title.png',
+  '/images/web2016profile/13-h-kis.jpeg',
+  '/images/web2016profile/14-t-matsumana.png',
+  '/images/web2016profile/17-h-tarunon.jpg',
+  '/images/web2016profile/13-k-yasukawa.png',
+  '/images/web2016profile/15-h-taro.jpg',
+  '/images/web2016profile/17-k-maki.jpg',
+  '/images/web2016profile/13-t-nagamine.jpg',
+  '/images/web2016profile/15-t-colun.jpg	',
+  '/images/web2016profile/17-t-sato.png',
+  '/images/web2016profile/14-h-yoichiro.jpeg',
+  '/images/web2016profile/16-h-yokoyama.jpg',
+  '/images/web2016profile/18-k-hamamoto.jpg',
+  '/images/web2016profile/14-k-urushibara.jpg',
+  '/images/web2016profile/16-t-takesako.jpg'];
 
-self.addEventListener('install', function(event) {
-  DOMAIN + event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return Promise.all(CACHE_URLS.map(function(url) {
-        return fetch(new Request(url)).then(function(response) {
-          if (response.ok){
-            return cache.put(response.url, response);
-          }else{
-            return Promise.reject('Invalid response.  URL:' + response.url + ' Status: ' + response.status);
-          }
-      });
-    }));
-  }));
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(CACHE_FILES);
+    })
+  )
 });
 
-self.addEventListener('fetch', function(event) {
-  if (CACHE_FLAG_LIST[event.request.url]){
-    event.respondWith(caches.match(event.request, {cacheName: CACHE_NAME}));
-  }
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
 self.addEventListener('activate', function(event) {
   event.waitUntil(
